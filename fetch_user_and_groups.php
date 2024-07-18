df55c7b36415070556858cd9f47ba648a2a25425
berhan <berhan5378@gmail.com>
2024-07-18 18:12:30 +0300

    fetch user and groups

--- fetch_user_and_groups.php
+++ fetch_user_and_groups.php
@@ -9,11 +9,16 @@ function fetchUserAndGroups(EntityManagerInterface $em, $users)
 {
    echo "\n","Users", "\t\t", "Groups" ,"\n";
    echo "------". "\t\t". "-----". "\n";
+   try {
     foreach($users as $user){
         foreach ($user->getGroups() as $group){  // get the group for user
-            echo $user->getName(),"\t\t" ,$group->getName(),"\n";
+            echo $user->getName(),"\t\t" ,$group->getName().':',"\n";
         }
     } 
+  } catch (\Exception $e) {
+      echo "An error occurred: " . $e->getMessage();
+  }
+    
     echo "\n";
 }
 $UserRepository = $entityManager->getRepository(User::class);
