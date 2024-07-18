dad762323c43e47ac4706136089f0afb4839fb0b
berhan <berhan5378@gmail.com>
2024-07-18 18:18:29 +0300

    user entity

--- src/App/Entity/User.php
+++ src/App/Entity/User.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\Collection;
 
 /**
  * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
  * @ORM\Table(name="users")
  */
 class User
@@ -15,7 +16,7 @@ class User
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
+     * @ORM\Column(name="id",type="integer")
      */
     private $id;
 
@@ -28,14 +29,23 @@ class User
      * @ORM\Column(type="string")
      */
     private $email;
-
+    
+     /**
+     * @ORM\Column(type="datetime", options={"default": "CURRENT_TIMESTAMP"})
+     */
+    private $createdAt;
+   
+     /**
+     * @ORM\Column(type="datetime", options={"default": "CURRENT_TIMESTAMP"})
+     */
+   private $updatedAt;
+    
     /**
      * @ORM\ManyToMany(targetEntity="UserGroup", inversedBy="users")
      * @ORM\JoinTable(name="users_groups")
      */
     private $groups;
-
-    // Add this property to the User class
+ 
     /**
     * @ORM\OneToMany(targetEntity="Post", mappedBy="user")
     */
@@ -100,5 +110,61 @@ class User
         }
 
         return $this;
+    }
+    // Lifecycle callback methods
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function onPrePersist()
+    {
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = new \DateTime();
+    }
+
+    /**
+     * @ORM\PreUpdate
+     */
+    public function onPreUpdate()
+    {
+        $this->updatedAt = new \DateTime();
+    }
+
+    // Getter and setter methods for createdAt and updatedAt
+
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function getUpdatedAt(): \DateTime
+    {
+        return $this->updatedAt;
+    }
+    /**
+     * @ORM\PostPersist
+     */
+    public function postPersist()
+    {
+        // Logic to execute after the entity is persisted
+        echo "User has been persisted with ID: " . $this->id . "\n";
+    }
+
+    /**
+     * @ORM\PostUpdate
+     */
+    public function postUpdate()
+    {
+        // Logic to execute after the entity is updated
+        echo "User with ID: " . $this->id . " has been updated.\n";
+    }
+
+    /**
+     * @ORM\PostRemove
+     */
+    public function postRemove()
+    {
+        // Logic to execute after the entity is removed
+        echo "User with ID: " . $this->id . " has been removed.\n";
     }
 }
